59de6f934e5f46954640c4747bad3d0a2f94ae1d
Saideep Somineni <saideep.somineni@kcc.com>
2025-09-16 12:38:58 +0530

    new commit

--- index.js
+++ index.js
@@ -20,8 +20,39 @@ $(document).on("click", ".back-to-top", function () {
         $("html, body").animate({
             scrollTop: target.offset().top - 150
         }, 600);
-    } else {
-        console.warn("Target element not found.");
     }
 });
 
+$(document).on("click", ".nav-item-about", function () {
+    const target = $("#about");
+    if (target.length) {
+        $("html, body").animate({
+            scrollTop: target.offset().top
+        }, 600);
+    }
+});
+
+$(document).on("click", ".nav-item-exp", function () {
+    const target = $("#experience");
+    if (target.length) {
+        $("html, body").animate({
+            scrollTop: target.offset().top
+        }, 600);
+    }
+});
+
+$(window).on("load", function () {
+    $(".nav-tabs li").on("click", function (e) {
+        e.preventDefault();
+        var activeTab = $(this).find("a").attr("href");
+        var selectedTab = $(this).find("a")
+        var hiddenTab = $(this).siblings().find("a");
+        if (!$(this).hasClass("active")) {
+            $(this).addClass("active").siblings().removeClass("active");
+            $(".tab-pane").removeClass("active");
+            $(activeTab).addClass("active");
+            $(selectedTab).addClass("active");
+            $(hiddenTab).removeClass("active");
+        }
+    });
+});
