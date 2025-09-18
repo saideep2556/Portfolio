80c52defaaff72cb8bd7aa21c06a6051542cf65b
Saideep Somineni <saideep.somineni@kcc.com>
2025-09-18 22:19:48 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// $(document).ready(function(){
-//     let $container = $("#name-text");
-//     let words = $container.text().trim().split(" ");
-//     $container.empty();
-//     $.each(words, function(i, word){
-//     let $span = $("<span class='word'>" + word + " </span>");
-//     $container.append($span);
-//     // Fade + slide effect with delay for each word
-//     $span.delay(i * 400).animate(
-//         {opacity: 1, top: "-10px"},
-//         600
-//     );
-//     });
-// });
-
-
 $(document).on("click", ".back-to-top", function () {
     const target = $(".home.container");
     if (target.length) {
@@ -34,6 +18,15 @@ $(document).on("click", ".nav-item-about", function () {
 
 $(document).on("click", ".nav-item-exp", function () {
     const target = $("#experience");
+    if (target.length) {
+        $("html, body").animate({
+            scrollTop: target.offset().top
+        }, 600);
+    }
+});
+
+$(document).on("click", ".nav-item-footer", function () {
+    const target = $("#footer");
     if (target.length) {
         $("html, body").animate({
             scrollTop: target.offset().top
